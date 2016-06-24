ad584b7e47330dee7dd7a6d1c3c994e3a8033af9
Trezy <tre@trezy.com>
2016-06-24 00:14:05 -0500

    Dynamically generate board based on data model

--- js/App2.jsx
+++ js/App2.jsx
@@ -5,8 +5,8 @@ import ReactDOM from 'react-dom'
 import BoardDataModel from './boardDataModel.json'
 
 window.onload = function () {
-  let height = 300
-  let width = 400
+  let height = window.innerHeight
+  let width = window.innerWidth
   let fov = 45
   let aspect = width / height
   let near = 0.1
@@ -19,7 +19,7 @@ window.onload = function () {
 
   scene.add(camera)
 
-  camera.position.z = 300
+  camera.position.z = 1000
 
   renderer.setSize(width, height)
 
@@ -30,22 +30,33 @@ window.onload = function () {
   let rings = 16
 
   let boxDimension = 100
-  let boxGeometry = new THREE.BoxGeometry(boxDimension, boxDimension, boxDimension * 2)
-  let boxMaterial = new THREE.MeshLambertMaterial({color: 0xCC0000})
-  let box = new THREE.Mesh(boxGeometry, boxMaterial)
+  let boxGeometry = new THREE.BoxGeometry(boxDimension * 5, boxDimension, boxDimension / 2)
+  let materials = {}
+
+  for (let i = 0; i < BoardDataModel.strips.length; i++) {
+    let strip = BoardDataModel.strips[i]
+
+    let boxMaterial = materials[strip.wood] = materials[strip.wood] ? materials[strip.wood] : new THREE.MeshLambertMaterial({
+      map: new THREE.TextureLoader().load(`/assets/woods/${strip.wood}.jpg`)
+    })
+
+    let box = new THREE.Mesh(boxGeometry, boxMaterial)
+
+    box.position.z = ((boxDimension / 2) * i) - (Math.round(BoardDataModel.strips.length / 2) * (boxDimension / 2))
+
+    scene.add(box)
+  }
 
   camera.position.x = -500
   camera.position.y = 500
-  camera.lookAt(box.position)
+  camera.lookAt(0, 0, 0)
 
   let controls = new THREE.OrbitControls(camera, renderer.domElement)
   controls.enableDamping = true
   controls.dampingFactor = 0.25
-  controls.enableZoom = false
+//  controls.enableZoom = false
 
-  scene.add(box)
-
-  let light0 = new THREE.DirectionalLight(0xffffff)
+  let light0 = new THREE.DirectionalLight('white')
   light0.position.set(1, 1, 1)
   scene.add(light0)
 
@@ -53,9 +64,9 @@ window.onload = function () {
   light1.position.set(-1, -1, -1)
   scene.add(light1)
 
-  scene.add(new THREE.AmbientLight(0xFFFFFF))
+  scene.add(new THREE.AmbientLight('white'))
 
-  renderer.setClearColor(0xdddddd, 1)
+  renderer.setClearColor('white', 1)
   renderer.render(scene, camera)
 
   let render = function () {
