a0880944c15928cad646aeb1c89c2fbb4df88903
Brian M. Herold <brian.herold@inin.com>
2016-06-30 16:45:02 -0500

    rezise working and more UI fanciness

--- js/App2.jsx
+++ js/App2.jsx
@@ -10,10 +10,18 @@ import BoardDataModel from './boardDataModel.json'
 import Board from 'views/Board'
 
 window.onload = function () {
-  window.board = new Board(BoardDataModel)
+  window.board = new Board(BoardDataModel, 'main')
+
+  window.addEventListener( 'resize', onWindowResize, false );
+  function onWindowResize(){
+      window.board.resize();
+      // camera.aspect = window.innerWidth / window.innerHeight;
+      // camera.updateProjectionMatrix();
+      // renderer.setSize( window.innerWidth, window.innerHeight );
+  }
 
 
-  // So sorry!
+  // Some bullshit for the board UI mockups
   $('.swatch-clickable').each(function () {
     $(this).attr('title', $(this).attr('id'));
   });
@@ -23,10 +31,36 @@ window.onload = function () {
     let woodClass = $(this).attr('class').split(' ').pop();
 
     $("#preview").attr('class', ['swatch', woodClass].join(' '));
-    $('.swatch-mini').attr('class', ['swatch swatch-mini', woodClass].join(' '));
+    $('.swatch-mini').first().attr('class', ['swatch swatch-mini', woodClass].join(' '));
     $("#color-name").html($(this).attr("id"));
     $("#color-field").val($(this).attr("id"))
   });
 
+
+  $('.next-step').click(function() {
+      $('section[type="step"]').each(function() {
+          debugger;
+          if ($(this).offset().left > $('body').width()) { //offscreen
+              $(this).animate({
+                  left: '0',
+                  right: '0'
+
+              }, 500 );
+          } else {
+              $(this).animate({ //onscreen
+                  left: '150%',
+                  right: '-150%'
+              }, 500 );
+          }
+
+      });
+
+      $('menu[type="toolbar"]').animate({
+        scrollTop: 0
+      }, 'fast');
+
+      // $('menu[type="toolbar"]').scrollTop(0); // immediate scroll
+
+  });
 
 }
--- js/views/Board.js
+++ js/views/Board.js
@@ -13,33 +13,33 @@ export default class Board {
     Public Methods
   \******************************************************************************/
 
-  constructor (data) {
+  constructor (data, container) {
     this.data = data
-    this.el = document.querySelector('main')
-    this.height = this.el.offsetHeight
-    this.materials = {}
-    this.width = this.el.offsetWidth
+    this.container = container
 
+    this.renderer = new THREE.WebGLRenderer
+    this.resize()
+
+    this.materials = {}
     this.boxDimension = 100
     this.boxGeometry = new THREE.BoxGeometry(this.boxDimension * 5, this.boxDimension, this.boxDimension / 2)
     this.far = 10000
     this.fov = 45
     this.near = 0.1
 
+
     this.scene = new THREE.Scene
     this.camera = new THREE.PerspectiveCamera(this.fov, this.width / this.height, this.near, this.far)
-    this.renderer = new THREE.WebGLRenderer
     this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement)
 
     this.camera.position.z = 1000
     this.camera.position.x = -500
-    this.camera.position.y = 500
+    this.camera.position.y = 1000
     this.camera.lookAt(0, 0, 0)
 
     this.controls.enableDamping = true
     this.controls.dampingFactor = 0.25
 
-    this.renderer.setSize(this.width, this.height)
     this.renderer.setClearColor('white', 1)
 
     this.scene.add(this.camera)
@@ -47,6 +47,19 @@ export default class Board {
     this.strips = new Backbone.Collection(data.strips)
 
     this.render()
+  }
+
+  resize () {
+    this.el = document.querySelector(this.container)
+    this.height = this.el.offsetHeight
+    this.width = this.el.offsetWidth
+
+    if (this.camera) {
+      this.camera.aspect = this.width / this.height;
+      this.camera.updateProjectionMatrix();
+    }
+
+    this.renderer.setSize(this.width, this.height);
   }
 
   render () {
