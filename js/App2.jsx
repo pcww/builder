f82ef6a3bc512cd112cb36d1cdf409e8454947e7
Brian M. Herold <brian.herold@inin.com>
2016-06-30 00:48:29 -0500

    Mocked strips into UI and updated woods

--- js/App2.jsx
+++ js/App2.jsx
@@ -11,4 +11,22 @@ import Board from 'views/Board'
 
 window.onload = function () {
   window.board = new Board(BoardDataModel)
+
+
+  // So sorry!
+  $('.swatch-clickable').each(function () {
+    $(this).attr('title', $(this).attr('id'));
+  });
+  $('.swatch-clickable').tooltip();
+
+  $(".swatch-clickable").click(function() {
+    let woodClass = $(this).attr('class').split(' ').pop();
+
+    $("#preview").attr('class', ['swatch', woodClass].join(' '));
+    $('.swatch-mini').attr('class', ['swatch swatch-mini', woodClass].join(' '));
+    $("#color-name").html($(this).attr("id"));
+    $("#color-field").val($(this).attr("id"))
+  });
+
+
 }
