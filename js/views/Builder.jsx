ed175d83f81a0420eb673b41356dc7f1fbdb0360
Brian Herold <bmherold@gmail.com>
2016-07-19 18:57:49 -0500

    Getting Close!

--- js/views/Builder.jsx
+++ js/views/Builder.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import BoardModel from 'models/Board'
+import Board from 'views/Board.jsx'
 import Wizard from 'views/Wizard.jsx'
-import Board from 'views/Board.js'
 
 export default class Builder extends React.Component {
   constructor (props) {
@@ -16,14 +16,6 @@ export default class Builder extends React.Component {
     this.request = this.state.board.fetch()
     .done(() => {
       this.setState({loaded: true})
-
-      // Init Three.js board
-      window.board = new Board(this.state.board, 'main')
-      window.addEventListener('resize', onWindowResize, false);
-      function onWindowResize() {
-        window.board.resize()
-      }
-
     })
   }
 
@@ -33,22 +25,17 @@ export default class Builder extends React.Component {
 
   render () {
     if (this.state.loaded) {
-      let strips = this.state.board.get('strips')
-      let width = this.state.board.get('width')
-      console.log(this.state.board)
       return (
-        <div>
-          <main class="board-view"></main>
-          <section type="overlay">
-            <div class="dimensions badge">Dimensions 38.6" x 25.0"</div>
-            <div class="cost well text-center">Board Cost: <span class="label label-success">$297.36</span></div>
-          </section>
-
-        </div>
+        <main>
+          <Board board={this.state.board} overlay />
+          <Wizard board={this.state.board} />
+        </main>
       )
     }
 
-    return <div>Loading that sexy board data...</div>
+    return (
+      <main><div class="loading">Loading that sexy board data...</div></main>
+    )
 
   }
 }
