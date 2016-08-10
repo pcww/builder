b901bcdeaecf83e6780833e16cc733fd24d74f1d
tpeebles <trezy.peebles@gettyimages.com>
2016-08-09 21:31:45 -0500

    Update board model width when the strip length slider changes

--- js/views/Wizard.jsx
+++ js/views/Wizard.jsx
@@ -5,6 +5,10 @@ import Step from 'views/Step.jsx'
 import StripPanel from 'views/StripPanel.jsx'
 
 export default class Wizard extends React.Component {
+  onStripLengthChanged (event) {
+    this.props.board.set('width', event.currentTarget.value)
+  }
+
   render () {
     let board = this.props.board
 
@@ -20,7 +24,13 @@ export default class Wizard extends React.Component {
           <fieldset>
             <legend>Strip Length</legend>
 
-            <input type="range" max="48" min="8" step="2" defaultValue={board.get('width')} />
+            <input
+              type="range"
+              max="48"
+              min="8"
+              step="2"
+              defaultValue={board.get('width')}
+              onChange={this.onStripLengthChanged.bind(this)} />
           </fieldset>
 
           <fieldset>
