b7cb0924b47bb6ceae751ece50d498c5cc4bacda
tpeebles <trezy.peebles@gettyimages.com>
2016-08-09 23:16:04 -0500

    Merge I think?

--- js/collections/Strips.js
+++ js/collections/Strips.js
@@ -3,7 +3,13 @@ import BaseCollection from 'collections/Base'
 
 export default class Strips extends BaseCollection {
   _bindEvents () {
-    this.on('change', function (model, options) {
+    this.on('change:size', (model, options) => {
+      this.collection.forEach(strip => {
+        strip.set('rendered', false)
+      })
+    })
+
+    this.on('change:wood', (model, options) => {
       if (!model.changed.rendered) {
         model.set('rendered', false)
       }
