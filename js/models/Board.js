6240af2f9ba61d14517c6a7e8c94064b3854b6c0
tpeebles <trezy.peebles@gettyimages.com>
2016-08-22 20:16:47 -0500

    Update dimensions in the overlay

--- js/models/Board.js
+++ js/models/Board.js
@@ -2,25 +2,56 @@ import Backbone from 'backbone'
 import BaseModel from 'models/Base'
 import StripsCollection from 'collections/Strips'
 
+
+
+
+
+let sizes = {
+  xxsmall: 0.25,
+  xsmall: 0.375,
+  small: 0.5,
+  medium: 0.625,
+  large: 1
+}
+
+
+
+
+
 export default class Board extends BaseModel {
   _bindEvents () {
-    this.on('change:width', () => {
+    this.listenTo(this, 'change:width', () => {
       this.get('strips').forEach(strip => {
         strip.set('rendered', false)
       })
     })
+
+    this.listenTo(this.get('strips'), 'change:size', this._updateLength)
+    this.listenTo(this, 'sync', this._updateLength)
+  }
+
+  _updateLength () {
+    let length = 0
+
+    this.get('strips').forEach(strip => {
+      length += sizes[strip.get('size')]
+    })
+
+    this.set('length', length)
   }
 
   constructor (data, options) {
     super(data, options)
+    this._updateLength()
     this._bindEvents()
-    this.set('strips', new StripsCollection)
   }
 
   get defaults () {
     return {
+      length: 0,
       name: 'test',
-      strips: []
+      strips: new StripsCollection,
+      width: 0
     }
   }
 
--- js/views/Board.jsx
+++ js/views/Board.jsx
@@ -4,16 +4,30 @@ import VirtualBoard from 'views/VirtualBoard.js'
 
 export default class Builder extends React.Component {
   constructor (props) {
-      super(props)
+    super(props)
+
+    this.props.board.on('change', (model, option) => {
+      this.setState({
+        length: model.get('length'),
+        width: model.get('width')
+      })
+    })
   }
 
   componentDidMount () {
-      // Init Three.js board
-      window.vboard = new VirtualBoard(this.props.board, '.virtual-board')
-      window.addEventListener('resize', onWindowResize, false)
-      function onWindowResize() {
-        window.vboard.resize()
-      }
+    // Init Three.js board
+    window.vboard = new VirtualBoard(this.props.board, '.virtual-board')
+    window.addEventListener('resize', onWindowResize, false)
+    function onWindowResize() {
+      window.vboard.resize()
+    }
+  }
+
+  componentWillMount () {
+    this.setState({
+      length: this.props.board.get('length'),
+      width: this.props.board.get('width')
+    })
   }
 
   render () {
@@ -21,7 +35,7 @@ export default class Builder extends React.Component {
     if (this.props.overlay) {
       overlay = (
         <section type="overlay">
-          <div className="dimensions badge">Dimensions 38.6" x 25.0"</div>
+          <div className="dimensions badge">Dimensions {this.state.length}" x {this.state.width}"</div>
           <div className="cost well text-center">Board Cost: <span className="label label-success">$297.36</span></div>
         </section>
       )
