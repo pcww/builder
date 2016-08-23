d73aa8719129797e06906d8d516d9d7f11ec10c0
tpeebles <trezy.peebles@gettyimages.com>
2016-08-22 19:33:51 -0500

    Rerender the entire board on wood change

--- js/collections/Strips.js
+++ js/collections/Strips.js
@@ -3,14 +3,10 @@ import BaseCollection from 'collections/Base'
 
 export default class Strips extends BaseCollection {
   _bindEvents () {
-    this.on('change:size', (model, options) => {
+    this.on('change:size change:wood', (model, options) => {
       this.forEach(strip => {
         strip.set('rendered', false)
       })
-    })
-
-    this.on('change:wood', (model, options) => {
-      model.set('rendered', false)
     })
   }
 
