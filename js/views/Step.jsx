71a6e3e52ee2ac9d67e2655c6618762533a725df
tpeebles <trezy.peebles@gettyimages.com>
2016-08-23 22:26:12 -0500

    Next and previous buttons work, mofo! 🎉

--- js/views/Step.jsx
+++ js/views/Step.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
 import classNames from 'classnames'
-import StepHeader from 'views/StepHeader.jsx'
 
 export default class Step extends React.Component {
   render () {
-    let currentStep = this.props.steps[this.props.index]
     let classes = classNames('step', {
-      'hidden': !currentStep.active
+      hidden: !this.props.isActive
     });
 
     return (
-      <section className={classes}>
-        <StepHeader heading={currentStep.name} index={this.props.index} steps={this.props.steps}></StepHeader>
-
-        <form>{this.props.children}</form>
-      </section>
+      <form className={classes}>
+        {this.props.children}
+      </form>
     )
   }
 }
--- js/views/StepHeader.jsx
+++ js/views/StepHeader.jsx
@@ -2,27 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 export default class StepHeader extends React.Component {
-  nextStep () {
-    this.props.steps[this.props.index].active = false
-    this.props.steps[parseInt(this.props.index) + 1].active = true
-  }
-
-  prevStep () {
-    this.props.steps[this.props.index].active = false
-    this.props.steps[parseInt(this.props.index) - 1].active = true
-  }
-
   render () {
-
     var nextButton = (
       <a href="#" className="next-step">
-        <i className="fa fa-chevron-circle-right" aria-hidden="true" onClick={this.nextStep.bind(this)}></i>
+        <i className="fa fa-chevron-circle-right" aria-hidden="true" onClick={this.props.onNext}></i>
       </a>
     )
 
     var previousButton = (
       <a href="#">
-        <i className="fa fa-chevron-circle-left" aria-hidden="true" onClick={this.prevStep.bind(this)}></i>
+        <i className="fa fa-chevron-circle-left" aria-hidden="true" onClick={this.props.onPrevious}></i>
       </a>
     )
 
