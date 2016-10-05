a116bab9095b544cab96a6e9f23e1ac8b6344a17
Trezy <tre@trezy.com>
2016-10-04 19:38:33 -0500

    Selected strip now pops up and moves across the board while dragging

---
+++ js/models/Strip.js
@@ -0,0 +1,14 @@
+import Backbone from 'backbone'
+import BaseModel from 'models/Base'
+
+
+
+
+
+export default class Strip extends BaseModel {
+   _bindEvents () {}
+
+   initialize () {
+     this._bindEvents()
+   }
+}
