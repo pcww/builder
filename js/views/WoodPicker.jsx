15e517423715843d823dbfb25391efcff72288f4
Brian Herold <bmherold@gmail.com>
2017-06-26 23:45:47 -0500

    added in new tabbed wood picker

--- js/views/WoodPicker.jsx
+++ js/views/WoodPicker.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Tabs, Tab } from 'react-bootstrap'
 
 import woods from '../woods.json'
 
 export default class WoodPicker extends React.Component {
+
+  toggleChangeWoods () {
+    this.setState({
+      showWoods: !this.state.showWoods
+    })
+  }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      showWoods: false
+    }
+
+    this.toggleChangeWoods = this.toggleChangeWoods.bind(this)
+  }
+
+
+
   onChangeWood (event) {
     let value = event.currentTarget.id
 
     this.props.strip.set('wood', value)
     this.props.updateState({}) // using the passed in updateState forces this component and the StripPanel parent to re-render.
+    // this.toggleModal()
     this.forceUpdate()
   }
 
@@ -16,25 +36,75 @@ export default class WoodPicker extends React.Component {
     let currentWood = this.props.strip.get('wood')
     let previewClasses = ['swatch', 'swatch-big', currentWood].join(' ')
     let woodObj = woods[currentWood] || 'Unknown'
+    let WoodTabs = null
 
-    let Woods = Object.keys(woods).map((key, index) => {
-      let wood = woods[key]
-      let classes = ['swatch', 'swatch-clickable', wood.safeName].join(' ')
+    if (this.state.showWoods) {
 
-      return (
-        <li className={classes} id={wood.safeName} key={index} title={wood.name} onClick={this.onChangeWood.bind(this)} data-toggle="tooltip"/>
-      )
-    })
+      let genericWoods = _.filter(woods, (wood) => { return !wood.endgrain && !wood.mosaic })
+      let endgrainWoods = _.filter(woods, (wood) => { return wood.endgrain })
+      let mosaicWoods = _.filter(woods, (wood) => { return wood.mosaic })
+
+      console.log('genericWoods: ', genericWoods)
+
+      let GenericWoods = _.map(genericWoods, (wood, index) => {
+        let classes = ['swatch', 'swatch-clickable', wood.safeName].join(' ')
+        console.log('')
+
+        return (
+          <li className={classes} id={wood.safeName} key={index} title={wood.name} onClick={this.onChangeWood.bind(this)} data-toggle="tooltip"/>
+        )
+      })
+
+      let EndgrainWoods = _.map(endgrainWoods, (wood, index) => {
+        let classes = ['swatch', 'swatch-clickable', wood.safeName].join(' ')
+
+        return (
+          <li className={classes} id={wood.safeName} key={index} title={wood.name} onClick={this.onChangeWood.bind(this)} data-toggle="tooltip"/>
+        )
+      })
+
+      let MosaicWoods = _.map(mosaicWoods, (wood, index) => {
+        let classes = ['swatch', 'swatch-clickable', wood.safeName].join(' ')
+
+        return (
+          <li className={classes} id={wood.safeName} key={index} title={wood.name} onClick={this.onChangeWood.bind(this)} data-toggle="tooltip"/>
+        )
+      })
+
+      WoodTabs = (
+        <div className="wood-tabs">
+          <a className="fa fa-window-close close" onClick={this.toggleChangeWoods}></a>
+          <Tabs defaultActiveKey={1} id="wood-tabs-bootstrap">
+            <Tab eventKey={1} title="Basic">
+              <ul className="wood-swatches">
+                {GenericWoods}
+              </ul>
+            </Tab>
+            <Tab eventKey={2} title="Mosaic">
+              <ul className="wood-swatches">
+                {MosaicWoods}
+              </ul>
+            </Tab>
+            <Tab eventKey={3} title="Endgrain">
+              <ul className="wood-swatches">
+                {EndgrainWoods}
+              </ul>
+            </Tab>
+          </Tabs>
+        </div>
+        )
+
+
+    }
+
 
     return (
       <div className="wood-picker">
-        <div className="dropdown">
+        <div>
           <a id="preview" className={previewClasses} data-toggle="dropdown"></a>
           <span className="color-name">{woodObj.name}</span><br/>
-          <a className="dropdown-toggle btn btn-sm btn-primary" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Change Wood</a>
-          <ul className="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
-            {Woods}
-          </ul>
+          <a className="btn btn-sm btn-primary" onClick={this.toggleChangeWoods}>Change Wood</a>
+          { this.state.showWoods ? WoodTabs : null }
         </div>
       </div>
     )
