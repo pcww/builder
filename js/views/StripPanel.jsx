02db07fe66a33ba58e9ccc021021833cb765483b
Brian Herold <bmherold@gmail.com>
2017-07-10 18:38:15 -0500

    Fixed - Strip width and wood type tooltips broken #81

--- js/views/StripPanel.jsx
+++ js/views/StripPanel.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import StripHeader from 'views/StripHeader.jsx'
 import WoodPicker from 'views/WoodPicker.jsx'
+import { Tooltip, OverlayTrigger } from 'react-bootstrap'
 
 export default class StripPanel extends React.Component {
   onSizeChange (event) {
@@ -31,60 +31,71 @@ export default class StripPanel extends React.Component {
             <fieldset>
               <legend>Strip Width</legend>
 
-              <label className="radio-inline" data-toggle="tooltip" title="Extra-Extra Small &mdash; 1/4&quot;">
-                <input
-                  checked={this.props.strip.get('size') === 'xxsmall'}
-                  id={radioGroupPrefix + '-size-1'}
-                  name={radioGroupName}
-                  type="radio"
-                  value="xxsmall"
-                  onChange={this.onSizeChange.bind(this)} />
-                XXS
-              </label>
+              <OverlayTrigger key={this.props.id+'xxsmall'} placement="top" overlay={<Tooltip id="tooltip">Extra-Extra Small &mdash; 1/4&quot;</Tooltip>}>
+                <label className="radio-inline">
+                  <input
+                    checked={this.props.strip.get('size') === 'xxsmall'}
+                    id={radioGroupPrefix + '-size-1'}
+                    name={radioGroupName}
+                    type="radio"
+                    value="xxsmall"
+                    onChange={this.onSizeChange.bind(this)} />
+                  XXS
+                </label>
+              </OverlayTrigger>
 
-              <label className="radio-inline" data-toggle="tooltip" title="Extra Small &mdash; 3/8&quot;">
-                <input
-                  checked={this.props.strip.get('size') === 'xsmall'}
-                  id={radioGroupPrefix + '-size-2'}
-                  name={radioGroupName}
-                  type="radio"
-                  value="xsmall"
-                  onChange={this.onSizeChange.bind(this)} />
-                XS
-              </label>
+              <OverlayTrigger key={this.props.id+'xsmall'} placement="top" overlay={<Tooltip id="tooltip">Extra Small &mdash; 3/8&quot;</Tooltip>}>
+                <label className="radio-inline">
+                  <input
+                    checked={this.props.strip.get('size') === 'xsmall'}
+                    id={radioGroupPrefix + '-size-2'}
+                    name={radioGroupName}
+                    type="radio"
+                    value="xsmall"
+                    onChange={this.onSizeChange.bind(this)} />
+                  XS
+                </label>
+              </OverlayTrigger>
 
-              <label className="radio-inline" data-toggle="tooltip" title="Small &mdash; 1/2&quot;">
-                <input
-                  checked={this.props.strip.get('size') === 'small'}
-                  id={radioGroupPrefix + '-size-3'}
-                  name={radioGroupName}
-                  type="radio"
-                  value="small"
-                  onChange={this.onSizeChange.bind(this)} />
-                S
-              </label>
+              <OverlayTrigger key={this.props.id+'small'} placement="top" overlay={<Tooltip id="tooltip">Small &mdash; 1/2&quot;</Tooltip>}>
+                <label className="radio-inline">
+                  <input
+                    checked={this.props.strip.get('size') === 'small'}
+                    id={radioGroupPrefix + '-size-3'}
+                    name={radioGroupName}
+                    type="radio"
+                    value="small"
+                    onChange={this.onSizeChange.bind(this)} />
+                  S
+                </label>
+              </OverlayTrigger>
 
-              <label className="radio-inline" data-toggle="tooltip" title="Medium &mdash; 5/8&quot;">
-                <input
-                  checked={this.props.strip.get('size') === 'medium'}
-                  id={radioGroupPrefix + '-size-4'}
-                  name={radioGroupName}
-                  type="radio"
-                  value="medium"
-                  onChange={this.onSizeChange.bind(this)} />
-                M
-              </label>
+              <OverlayTrigger key={this.props.id+'medium'} placement="top" overlay={<Tooltip id="tooltip">Medium &mdash; 5/8&quot;</Tooltip>}>
+                <label className="radio-inline">
+                  <input
+                    checked={this.props.strip.get('size') === 'medium'}
+                    id={radioGroupPrefix + '-size-4'}
+                    name={radioGroupName}
+                    type="radio"
+                    value="medium"
+                    onChange={this.onSizeChange.bind(this)} />
+                  M
+                </label>
+              </OverlayTrigger>
 
-              <label className="radio-inline" data-toggle="tooltip" title="Large &mdash; 1&quot;">
-                <input
-                  checked={this.props.strip.get('size') === 'large'}
-                  id={radioGroupPrefix + '-size-5'}
-                  name={radioGroupName}
-                  type="radio"
-                  value="large"
-                  onChange={this.onSizeChange.bind(this)} />
-                L
-              </label>
+              <OverlayTrigger key={this.props.id+'large'} placement="top" overlay={<Tooltip id="tooltip">Large &mdash; 1&quot;</Tooltip>}>
+                <label className="radio-inline">
+                  <input
+                    checked={this.props.strip.get('size') === 'large'}
+                    id={radioGroupPrefix + '-size-5'}
+                    name={radioGroupName}
+                    type="radio"
+                    value="large"
+                    onChange={this.onSizeChange.bind(this)} />
+                  L
+                </label>
+              </OverlayTrigger>
+
             </fieldset>
 
             <hr/>
