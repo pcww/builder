5879d393ace7b682cb475bd8629232733d56a343
Trezy <tre@trezy.com>
2017-07-10 21:18:01 -0500

    [#44] Add modal for hi-res feet images

--- js/views/FeetPicker.jsx
+++ js/views/FeetPicker.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { Modal } from 'react-bootstrap'
+
 import accessories from '../accessories.json'
 
 export default class FeetPicker extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      modalIsOpen: false
+    }
+  }
+
   onFeetChange (event) {
     this.props.board.set('feet', {type: event.currentTarget.value})
     this.forceUpdate()
@@ -17,9 +27,24 @@ export default class FeetPicker extends React.Component {
 
     return (
       <div className="media">
-        <div className="media-left">
+        <div className="media-left" onClick={() => this.setState({ modalIsOpen: true })}>
           <img className="media-object swatch swatch-big" src={'/assets/feet/' + feet + '.jpg'} alt="..."/>
         </div>
+
+        <Modal show={this.state.modalIsOpen} onHide={() => this.setState({ modalIsOpen: false })}>
+          <Modal.Header closeButton>
+            <Modal.Title>{feetName}</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body style={{ textAlign: 'center' }}>
+            <img style={{ maxWidth: '100%' }} src={'/assets/feet/' + feet + '.jpg'} alt="..."/>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <p>{feetDescription}</p>
+          </Modal.Footer>
+        </Modal>
+
         <div className="media-body">
           <div className="row">
             <div className="col-xs-12">
